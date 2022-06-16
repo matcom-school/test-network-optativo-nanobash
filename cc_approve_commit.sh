f2ee931ca0c827b59194087132c4fa48d19fb7e2
Kmilo Denis Glez <kmilo.denis.glez@gmail.com>
2022-06-16 12:45:01 -0400

    modify cc_approve_commit.sh

--- cc_approve_commit.sh
+++ cc_approve_commit.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env sh
 set -eu
 
-# imports
-. ./env.sh
+echo "Este script es solo para el proceso de instalacion en la ORG-1"
+echo "Para instalar el chaincode en la ORG-2 no es necesario ejecutar este script"
+echo "Solo ejecutar:"
+echo "source peer2admin.sh"
+echo "peer lifecycle chaincode install mycc.tar.gz"
 
-export CORE_PEER_MSPCONFIGPATH="${PWD}"/crypto-config/organizations/org1.example.com/users/Admin@org1.example.com/msp
+# look for binaries in local dev environment /build/bin directory and then in local samples /bin directory
+export PATH="${PWD}"/../bin:"$PATH"
+export FABRIC_CFG_PATH="${PWD}"/../config
+
+export FABRIC_LOGGING_SPEC=INFO
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID=Org1MSP
+export CORE_PEER_ID=peer0.org1.example.com
+export CORE_PEER_MSPCONFIGPATH="${PWD}"/crypto-config/organizations/org1.example.com/peers/peer0.org1.example.com/msp
+export CORE_PEER_ADDRESS=127.0.0.1:7051
 
 peer lifecycle chaincode approveformyorg  -o 127.0.0.1:7050 --channelID mychannel --name mycc --version 1.0 --sequence 1 --signature-policy "OR ('Org1MSP.member')" --package-id mycc:1.0
 peer lifecycle chaincode checkcommitreadiness -o 127.0.0.1:7050 --channelID mychannel --name mycc --version 1.0 --sequence 1 --signature-policy "OR ('Org1MSP.member')"
